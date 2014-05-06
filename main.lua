0a3c7412345455d209e766fb443959e6ae40d5df
Paul Robson <paul@robsons.org.uk>
2014-05-06 13:47:48 +0100

    Improved main.lua

--- main.lua
+++ main.lua
@@ -1,12 +1,19 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+--- ************************************************************************************************************************************************************************
+---
+---				Name : 		main.lua
+---				Purpose :	Demo for transaction library.
+---				Created:	30 April 2014
+---				Author:		Paul Robson (paul@robsons.org.uk)
+---				License:	MIT
+---
+--- ************************************************************************************************************************************************************************
 
 
 local transitions = require("system.transitions")
 
+--
+--	Function creates a "scene" (a displayGroup) which is slightly different so I've something to work with
+--
 function createScene(n)
 	local grp = display.newGroup()
 	local square = display.newRect(160,n*100+100,130+n*70,200)
@@ -19,15 +26,15 @@ function createScene(n)
 	return grp
 end
 
-scene1 = createScene(1)
+scene1 = createScene(1)																			-- create two scenes
 scene2 = createScene(2)
 
-local recipient = {}
+local recipient = {} 																			-- create an object to receive the message
 
 function recipient:transitionCompleted()
-	scene1.isVisible = false
+	scene1.isVisible = false 																	-- hide the from scene. The system doesn't (deliberate)
 	print("Done !")
 end
 
-tt = "flip"
-transitions:execute(tt,recipient,scene1,scene2,1000)
+tt = "flip"																						-- name a transition
+transitions:execute(tt,recipient,scene1,scene2,1000) 											-- and do it.
